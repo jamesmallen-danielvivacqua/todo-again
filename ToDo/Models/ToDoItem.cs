bb70e9a60073adabcc91c8bd0243058d919949c7
danielvivacqua <drvivacqua@hotmail.com>
2016-08-16 11:12:45 -0400

    initial commit

---
+++ ToDo/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class ToDoItem
+    {
+        public int ToDoItemID { get; set; }
+        public string ToDoText { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
